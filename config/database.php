5438a1cbcb5158eb3c7f2df8705a417a39f09cfb
unknown <fatur_muhammad@bk.ru>
2023-01-18 15:46:09 +0700

    added mongodb db config

--- config/database.php
+++ config/database.php
@@ -91,6 +91,11 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb+srv://username:password@<atlas-cluster-uri>/myappdb?retryWrites=true&w=majority'),
+            'database' => 'myappdb',
+        ],
     ],
 
     /*
