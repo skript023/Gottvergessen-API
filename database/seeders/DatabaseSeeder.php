1bd01ea47fb528a7d3d8ef90e465ca3c17ccc290
skript023 <fatur_muhammad@bk.ru>
2023-01-23 19:50:36 +0700

    Add client monitor seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(BinarySeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(ClientMonitor::class);
     }
 }
