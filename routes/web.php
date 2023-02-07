ab2926109048b61ac6afff6f78685ea0e4ce4db5
unknown <fatur_muhammad@bk.ru>
2023-02-07 16:14:55 +0700

    Fix user delete

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::group(['middleware' => ['auth', 'admin', 'verified']], function()
     Route::get('/dashboard/users', [UserController::class, 'view_user']);
     Route::post('/dashboard/users/add', [UserController::class, 'add_user']);
     Route::post('/dashboard/users/update/{selected_user}', [UserController::class, 'update_user']);
-    Route::post('/dashboard/users/delete/{selected_user}', [UserController::class, 'update_user']);
+    Route::post('/dashboard/users/delete/{selected_user}', [UserController::class, 'delete_user']);
     
     Route::get('/dashboard/logging',[ClientMonitorController::class, 'load_logs']);
     Route::get('/dashboard/logs/delete/{id}', [ClientMonitorController::class, 'delete_log']);
