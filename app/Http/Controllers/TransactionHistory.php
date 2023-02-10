c1b4236b9820131b1920a706c86e4274781cd581
unknown <fatur_muhammad@bk.ru>
2023-02-10 15:03:23 +0700

    Add Transaction Histroy

--- app/Http/Controllers/TransactionHistory.php
+++ app/Http/Controllers/TransactionHistory.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\transaction;
 use Illuminate\Http\Request;
 
 class TransactionHistory extends Controller
 {
     public function index()
     {
-        return view("transaction-history");
+        $outcomes = transaction::all()->get('outcome');
+        $transaction = transaction::all()->first();
+
+        $result = 0;
+
+        foreach ($outcomes as $outcome)
+        {
+            $result = $outcome + $outcome;
+        }
+
+        return view("dashboard.transaction-history", [
+            'transactions' => $transaction,
+            'total_outcome' => $result
+        ]);
+    }
+
+    public function overall_outcome(Request $request)
+    {
+        
     }
 }
