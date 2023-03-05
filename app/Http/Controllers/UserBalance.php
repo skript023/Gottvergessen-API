ff35003819d8e4a7e8540a948f99ca294eb0723f
skript023 <fatur_muhammad@bk.ru>
2023-03-05 18:30:16 +0700

    Added balance controller

--- app/Http/Controllers/UserBalance.php
+++ app/Http/Controllers/UserBalance.php
@@ -13,4 +13,29 @@ class UserBalance extends Controller
             'balances' => balance::all()
         ]);
     }
+
+    public function user_balance()
+    {
+        return view('dashboard.user_balance', [
+            'balance' => balance::where('user_id', auth()->user()->id)->first()
+        ]);
+    }
+
+    public function add_balance(Request $request)
+    {
+        $balance = balance::where('user_id', $request->user)->where('wallet_id', $request->wallet);
+
+        $balance->amount += $request->amount;
+
+        $balance->save();
+    }
+
+    public function remove_balance(Request $request)
+    {
+        $balance = balance::where('user_id', $request->user)->where('wallet_id', $request->wallet);
+
+        $balance->amount -= $request->amount;
+
+        $balance->save();
+    }
 }
