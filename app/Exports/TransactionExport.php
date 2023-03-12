9a79da8c1fd07aeaa719100d19e33a16852d458c
skript023 <fatur_muhammad@bk.ru>
2023-03-12 11:36:57 +0700

    Add Excel Convert

--- app/Exports/TransactionExport.php
+++ app/Exports/TransactionExport.php
@@ -3,15 +3,41 @@
 namespace App\Exports;
 
 use App\Models\transaction;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Excel;
 
-class TransactionExport implements FromCollection
+class TransactionExport implements FromView, Responsable
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    use Exportable;
+
+    /**
+    * It's required to define the fileName within
+    * the export class when making use of Responsable.
+    */
+    private $fileName = 'invoices.xlsx';
+    
+    /**
+    * Optional Writer Type
+    */
+    private $writerType = Excel::XLSX;
+    
+    /**
+    * Optional headers
+    */
+    private $headers = [
+        'Content-Type' => 'text/csv',
+    ];
+
+    public function view() : View
     {
-        return transaction::all();
+        return view('dashboard.transactions.report', [
+            'transactions' => transaction::all()
+        ]);
     }
 }
---
+++ resources/views/dashboard/transactions/report.blade.php
@@ -0,0 +1,26 @@
+<table>
+    <thead>
+    <tr>
+        <th>Judul</th>
+        <th>Keterangan</th>
+        <th>Kantor</th>
+        <th>Jenis Uang</th>
+        <th>Pengeluaran</th>
+        <th>Pemasukan</th>
+        <th>Tanggal</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($transactions as $transaction)
+        <tr>
+            <td>{{ $transaction->title }}</td>
+            <td>{{ $transaction->description }}</td>
+            <td>{{ $transaction->office }}</td>
+            <td>{{ $transaction->type }}</td>
+            <td>{{ $transaction->expenditure }}</td>
+            <td>{{ $transaction->income }}</td>
+            <td>{{ $transaction->transaction_date }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
