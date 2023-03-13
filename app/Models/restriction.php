c80ab4fd730cfce0817cf5add323d05b26474c04
skript023 <fatur_muhammad@bk.ru>
2023-03-13 21:26:20 +0700

    add date picker

--- app/Models/restriction.php
+++ app/Models/restriction.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class restriction extends Model
 {
     use HasFactory;
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'route',
+        'level',
+        'role_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+    ];
 }
